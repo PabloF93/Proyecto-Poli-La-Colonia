87f1ee3c75ae50fcf078bead59a9c39b27e70778
PabloF93 <metalminds9387@gmail.com>
2018-05-13 19:42:55 -0300

    Inicio del proyecto

---
+++ src/proyectopolilacolonia/ProyectoPoliLaColonia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectopolilacolonia;
+
+/**
+ *
+ * @author Pablo2
+ */
+public class ProyectoPoliLaColonia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
