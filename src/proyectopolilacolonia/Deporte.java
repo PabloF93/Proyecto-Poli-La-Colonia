507cf5b2515c3daf7562d07d31f7fc58a8b11059
PabloF93 <metalminds9387@gmail.com>
2018-06-06 03:49:32 -0300

    Agregados los metodos list, find, save, delete a la clase Deporte

--- src/proyectopolilacolonia/Deporte.java
+++ src/proyectopolilacolonia/Deporte.java
@@ -5,8 +5,12 @@
  */
 package proyectopolilacolonia;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,6 +20,20 @@ public class Deporte extends Entidad{
 
     private String nombreDeporte;
     private List<Categoria> categoriaList = new ArrayList();
+    private Boolean deleted = false;
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+    
+    private Connection conn = null;
+    private ResultSet rs = null;
+    private PreparedStatement ps = null;
+    
 
     public List<Categoria> getA() {
         return categoriaList;
@@ -36,4 +54,75 @@ public class Deporte extends Entidad{
     public void setNombreDeporte(String nombreDeporte) {
         this.nombreDeporte = nombreDeporte;
     }       
+    
+    //Listar todos los deportes no eliminados.
+    public ResultSet list() {
+        conn = MySql.getConnection();
+        try {
+            ps = conn.prepareStatement("SELECT * FROM deportes WHERE deleted=false");
+            rs = ps.executeQuery();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: "+e.toString());
+        }
+        
+        return rs;
+    }
+    
+    //Retorna un deporte mediante el nombre.
+    public Deporte find(String nombre) {
+        Deporte d1 = new Deporte();
+        conn = MySql.getConnection();
+        try {
+            ps = conn.prepareStatement("SELECT * FROM deportes WHERE nombre=? AND deleted=false");
+            ps.setString(1, nombre);
+            
+            d1.setId(rs.getInt(1));
+            d1.setNombreDeporte(rs.getString(2));
+            
+            
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: "+e.toString());
+        }
+        
+        return d1;
+    }
+    
+    //Guarda el deporte.
+    public void save(Deporte deporte) {
+        conn = MySql.getConnection();
+        try {
+            ps = conn.prepareStatement("INSERT INTO deportes(nombre, deleted) VALUES(?,?)");
+            ps.setString(1, deporte.getNombreDeporte());
+            ps.setBoolean(2, deporte.getDeleted());
+            
+            int resultado = ps.executeUpdate();
+            if(resultado==1) {
+                JOptionPane.showMessageDialog(null, "¡Deporte guardado con éxito!");
+            }
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: "+e.toString());
+        }
+        
+        
+    }
+    
+    //Realiza un soft-delete del deporte.
+    public void delete(int id) {
+        conn = MySql.getConnection();
+        try {
+            ps = conn.prepareStatement("UPDATE deportes SET deleted=false WHERE id=?");
+            ps.setInt(1, id);
+            
+            int resultado = ps.executeUpdate();
+            if(resultado==1) {
+                JOptionPane.showMessageDialog(null, "¡Deporte eliminado con éxito!");
+            }
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: "+e.toString());
+        }
+    }
 }
